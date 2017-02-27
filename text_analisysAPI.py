f4c0d267029625e8d5287148a12ac2a75b4af8e4
Laszlo Szoboszlai <szlaci83@gmail.com>
2017-02-27 03:05:31 +0000

    API skeleton added

---
+++ text_analisysAPI.py
@@ -0,0 +1,33 @@
+from text_analyser import *
+
+#return the available currencies
+@app.route("/books", methods=['GET'])
+def get_books():
+    return jsonify(text_analyser.books)
+
+
+#expecting : { "bookname" : "BOOK.txt"} format
+@app.route("/meaning", methods=['POST', 'GET'])
+def meaning():
+    if not request.json or not 'bookname' in request.json:
+        abort(4)
+    bookname = request.json['bookname']
+    # response = call the right function to get response
+
+    return jsonify(response), 201
+
+
+
+#expecting : { "bookname" : "BOOK.txt"} format
+@app.route("/sentiment", methods=['POST', 'GET'])
+def sentiment():
+    if not request.json or not 'bookname' in request.json:
+        abort(4)
+    bookname = request.json['bookname']
+    #response = call the right function to get response
+
+    return jsonify(response), 201
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
